1ac883e7e00cac4fe04f9f64a77040571f132c40
Julian Bass <j.bass@salford.ac.uk>
2022-01-03 13:57:14 +0000

    Minor change to source code layout

--- CarTester/src/car/CarTester.java
+++ CarTester/src/car/CarTester.java
@@ -9,6 +9,7 @@ public class CarTester {
 		FastCar fastCar = new FastCar("BMW", "M320", 180);
 		FamilyCar familyCar = new FamilyCar("BMW", "320i Tourer", 5);
 
+		
 		System.out.println("Car " + car.getMake() + " " + car.getModel());
 		System.out.println("Fast Car " + fastCar.getMake() + " " + fastCar.getModel());
 		System.out.println("Family Car " + familyCar.getMake() + " " + familyCar.getModel());
